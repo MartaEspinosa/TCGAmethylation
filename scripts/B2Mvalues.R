f9ac3096f15b3f1e29722e4221ce306ce6f47ecb
MartaEspinosa <espinosacamarenamarta@gmail.com>
2023-10-19 16:39:56 +0200

    BetaValues & Mvalues

--- scripts/B2Mvalues.R
+++ scripts/B2Mvalues.R
@@ -5,15 +5,15 @@
 # This scripts passes from B-values to M-values
 
 # Run like this:
-# Rscript B2Mvalues.R 
+# Rscript B2Mvalues.R $inpath $outpath
 
 ######################## libraries to be loaded #################################
 library(dplyr)
 library(tidyr)
-library(ggplot2)
-library(ggthemes)
 library(data.table)
 library(lumi)
+options(warn=-1)
+
 ################################################################################
 
 ############################## reading inputs ##################################
@@ -29,18 +29,34 @@ if (length(args)<1) {
 inpath <- args[1]
 outpath <- args[2]
 
+# inpath <- "/users/genomics/marta/TCGA_methylation/analysis/0_bvalues/TCGA-BLCA"
+# outpath <- "/users/genomics/marta/TCGA_methylation/analysis/1_mvalues/TCGA-BLCA"
+dir.create(outpath, recursive = T)
+
 ################################################
 ### Read input
 ################################################
-bvalue_files = list.file(inpath, all.files=T, full.names=T)
-patientIDs = list.file(bvalue_files, all.files=T, full.names=F)
+bvalue_files = list.files(inpath, full.names=T)
+patientIDs = list.files(inpath, full.names=F)
 patientIDs = gsub("_betavalues.txt","",patientIDs)
-patientIDs
 
-for(i in 1:len(bvalue_files)) {
+for(i in 1:length(bvalue_files)) {
+  # name of outfile
+  outfile = paste0(outpath,"/",patientIDs[i],"_mvalues.txt")
+  
   # read beta-values
-  bvalue = fread(bvalue_files[i])
+  bvalue = as.data.frame(fread(bvalue_files[i]))
+  row.names(bvalue) = bvalue$CpG
+  bvalue$CpG = NULL
+  bvalue = as.matrix(bvalue)
+  
   # convert to m-values
-  mvalue = beta2m(bvalue)
-  print(mvalue)
+  mvalue = as.data.frame(beta2m(bvalue))
+  colnames(mvalue) = "Mvalues"
+  mvalue$CpG <- rownames(mvalue)
+  # reorder
+  mvalue <- mvalue[, c("CpG", "Mvalues")]
+  
+  # save output Mvalues
+  write.table(mvalue, outfile, row.names = F, quote=F, sep="\t")
 }
---
+++ scripts/oneFile_CancerType.R
@@ -0,0 +1,61 @@
+#!/usr/bin/env Rscript
+# Author: Marta Espinosa Camarena
+# email: mespinosa@imim.es
+# Date: 19th Oct 2023
+# This scripts passes from individual files with B or M values to merged ones. If a different number of CpGs are found, NAs are used to fill blank spaces
+
+# Run like this:
+# Rscript onFile_CancerType.R $inpath
+
+######################## libraries to be loaded #################################
+library(dplyr)
+library(tidyr)
+library(data.table)
+options(warn=-1)
+
+################################################################################
+
+############################## reading inputs ##################################
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)<1) {
+  stop("Please provide the path until the folder with the values", call.=FALSE)
+}
+################################################################################
+
+# Read arguments
+inpath <- args[1]
+# inpath <- "/users/genomics/marta/TCGA_methylation/analysis/0_bvalues/TCGA-BLCA"
+
+if (grepl("mvalues", inpath)) {
+  values <- "mvalues"
+} else if (grepl("bvalues", inpath)) {
+  values <- "bvalues"
+}
+
+################################################
+### Read input
+################################################
+files = list.files(inpath, full.names=T)
+patientIDs = list.files(inpath, full.names=F)
+if (values == "mvalues") {
+  patientIDs = gsub("_mvalues.txt","",patientIDs)
+} else if (values == "bvalues") {
+  patientIDs = gsub("_betavalues.txt","",patientIDs)
+}
+
+df = data.frame("CpG" = character(),
+                stringsAsFactors = F)
+
+for(i in 1:length(files)) {
+  # read file
+  input = as.data.frame(fread(files[i]))
+  names(input)[2] = patientIDs[i]
+  
+  # get one file where each column has the Bvalues per patient for all the CpGs. Keep NAs 
+  df = merge(df, input, by="CpG", all.x = T, all.y = T)
+  
+}
+
+write.csv(df, paste0(inpath,"/",values,".csv"), row.names =F, quote=F)
